c8f8e1c5f3eba8a549d2a170530f828f1596b967
dyvach94 <dyvach94@gmail.com>
2025-12-30 22:02:42 +0200

    2.2

--- js/game.js
+++ js/game.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:199a83563aa7473ed750e071b58df693d0b6cae610b5b5b9febf448e7c02b864
-size 2034771
+oid sha256:4b16fc31b83c1ef114bb82da5b49d1e073852ed96224c7d92ff43acd572469bf
+size 2050174
